9d19ca784a38b4de301aa01bba737f831d825686
blackkara <karamusti@gmail.com>
2015-07-20 02:37:19 +0300

    commit

--- routes/payment.js
+++ routes/payment.js
@@ -16,11 +16,36 @@ router.post('/payment', function(req, res, next){
         "orderFullName" : req.body.orderFullName
     };
 
-    
-    var payment = new Payment(paymentData);
+    try{
+        var payment = new Payment(paymentData);
+    } catch(error){
+
+        // If the error caused by programmer or database
+        // there is no need to send to consumer directly.
+        // For this reason, only i see above errors (in background)
+        // To see a "SERVER ERROR" for consumer is enough.
+        // Forexample, if a configuration file not found
+        // its not meaningful for consumer.
+        if(error.status === 500){
+            error.message = "SERVER ERROR";
+        }
+
+
+        if(error.status === 400){
+            // If the error caused by consumer (user),
+            // its meaningful to show error reason.
+            // Maybe he/she filled wrong values or
+            // their card type couldn't be recognised
+        }
+
+        return next(error);
+    }
 
     payment.makePayment(function (error, response) {
         if(error){
+            if(error.status === 500){
+                error.message = "SERVER ERROR"
+            }
             next(error);
         }else{
             res.send(response);
